1dd6261fb8b97c8c868f82f1865fa6427f01fef6
priyatrantik-cyber <priyatrantik@gmail.com>
2025-08-31 15:32:40 +0530

    Update task_manager.py

--- colab_leecher/utility/task_manager.py
+++ colab_leecher/utility/task_manager.py
@@ -1,5 +1,6 @@
 # copyright 2024 © Xron Trix | https://github.com/Xrontrix10
 
+
 import pytz
 import shutil
 import logging
@@ -39,25 +40,38 @@ from colab_leecher.utility.variables import (
     TaskError,
 )
 
-async def task_processor(task):
-    """
-    This function processes a single task from the queue.
-    It encapsulates all the logic for a single download/upload cycle.
-    """
-    
+
+async def task_starter(message, text):
+    global BOT
+    await message.delete()
+    BOT.State.started = True
+    if BOT.State.task_going == False:
+        src_request_msg = await message.reply_text(text)
+        return src_request_msg
+    else:
+        msg = await message.reply_text(
+            "I am already working ! Please wait until I finish !!"
+        )
+        await sleep(15)
+        await msg.delete()
+        return None
+
+
+async def taskScheduler():
+    global BOT, MSG, BotTimes, Messages, Paths, Transfer, TaskError
+    src_text = []
     is_dualzip, is_unzip, is_zip, is_dir = (
-        task.options.mode == "undzip",
-        task.options.mode == "unzip",
-        task.options.mode == "zip",
-        task.mode == "dir-leech",
+        BOT.Mode.type == "undzip",
+        BOT.Mode.type == "unzip",
+        BOT.Mode.type == "zip",
+        BOT.Mode.mode == "dir-leech",
     )
-    
-    # Reset Texts & Transfer data using the task object
+    # Reset Texts
     Messages.download_name = ""
     Messages.task_msg = f"<b>🦞 TASK MODE » </b>"
     Messages.dump_task = (
         Messages.task_msg
-        + f"<i>{task.options.mode.capitalize()} {task.mode.capitalize()} as {BOT.Setting.stream_upload}</i>\n\n<b>🖇️ SOURCES » </b>"
+        + f"<i>{BOT.Mode.type.capitalize()} {BOT.Mode.mode.capitalize()} as {BOT.Setting.stream_upload}</i>\n\n<b>🖇️ SOURCES » </b>"
     )
     Transfer.sent_file = []
     Transfer.sent_file_names = []
@@ -66,22 +80,20 @@ async def task_processor(task):
     Messages.download_name = ""
     Messages.task_msg = ""
     Messages.status_head = f"<b>📥 DOWNLOADING » </b>\n"
-    
-    src_text = []
 
     if is_dir:
-        if not ospath.exists(task.source[0]):
+        if not ospath.exists(BOT.SOURCE[0]):
             TaskError.state = True
             TaskError.text = "Task Failed. Because: Provided Directory Path Not Exists"
             logging.error(TaskError.text)
             return
         if not ospath.exists(Paths.temp_dirleech_path):
             makedirs(Paths.temp_dirleech_path)
-        Messages.dump_task += f"\n\n📂 <code>{task.source[0]}</code>"
-        Transfer.total_down_size = getSize(task.source[0])
-        Messages.download_name = ospath.basename(task.source[0])
+        Messages.dump_task += f"\n\n📂 <code>{BOT.SOURCE[0]}</code>"
+        Transfer.total_down_size = getSize(BOT.SOURCE[0])
+        Messages.download_name = ospath.basename(BOT.SOURCE[0])
     else:
-        for link in task.source:
+        for link in BOT.SOURCE:
             if is_telegram(link):
                 ida = "💬"
             elif is_google_drive(link):
@@ -113,6 +125,7 @@ async def task_processor(task):
 
     if ospath.exists(Paths.WORK_PATH):
         shutil.rmtree(Paths.WORK_PATH)
+        # makedirs(Paths.WORK_PATH)
         makedirs(Paths.down_path)
     else:
         makedirs(Paths.WORK_PATH)
@@ -131,11 +144,11 @@ async def task_processor(task):
             MSG.sent_msg = await MSG.sent_msg.reply_text(text=src_text[lin], quote=True)
 
     Messages.src_link = f"https://t.me/c/{Messages.link_p}/{MSG.sent_msg.id}"
-    Messages.task_msg += f"__[{task.options.mode.capitalize()} {task.mode.capitalize()} as {BOT.Setting.stream_upload}]({Messages.src_link})__\n\n"
+    Messages.task_msg += f"__[{BOT.Mode.type.capitalize()} {BOT.Mode.mode.capitalize()} as {BOT.Setting.stream_upload}]({Messages.src_link})__\n\n"
 
-    await task.status_msg.delete()
+    await MSG.status_msg.delete()
     img = Paths.THMB_PATH if ospath.exists(Paths.THMB_PATH) else Paths.HERO_IMAGE
-    task.status_msg = await colab_bot.send_photo(  # type: ignore
+    MSG.status_msg = await colab_bot.send_photo(  # type: ignore
         chat_id=OWNER,
         photo=img,
         caption=Messages.task_msg
@@ -145,27 +158,27 @@ async def task_processor(task):
         reply_markup=keyboard(),
     )
 
-    await calDownSize(task.source)
+    await calDownSize(BOT.SOURCE)
 
     if not is_dir:
-        await get_d_name(task.source[0])
+        await get_d_name(BOT.SOURCE[0])
     else:
-        Messages.download_name = ospath.basename(task.source[0])
+        Messages.download_name = ospath.basename(BOT.SOURCE[0])
 
     if is_zip:
         Paths.down_path = ospath.join(Paths.down_path, Messages.download_name)
         if not ospath.exists(Paths.down_path):
             makedirs(Paths.down_path)
 
-    task.start_time = datetime.now()
+    BotTimes.current_time = time()
 
-    if task.mode != "mirror":
-        await Do_Leech(task, task.source, is_dir, task.ytdl, is_zip, is_unzip, is_dualzip)
+    if BOT.Mode.mode != "mirror":
+        await Do_Leech(BOT.SOURCE, is_dir, BOT.Mode.ytdl, is_zip, is_unzip, is_dualzip)
     else:
-        await Do_Mirror(task, task.source, task.ytdl, is_zip, is_unzip, is_dualzip)
+        await Do_Mirror(BOT.SOURCE, BOT.Mode.ytdl, is_zip, is_unzip, is_dualzip)
 
 
-async def Do_Leech(task, source, is_dir, is_ytdl, is_zip, is_unzip, is_dualzip):
+async def Do_Leech(source, is_dir, is_ytdl, is_zip, is_unzip, is_dualzip):
     if is_dir:
         for s in source:
             if not ospath.exists(s):
@@ -174,24 +187,24 @@ async def Do_Leech(task, source, is_dir, is_ytdl, is_zip, is_unzip, is_dualzip):
                 return
             Paths.down_path = s
             if is_zip:
-                await Zip_Handler(task, Paths.down_path, True, False)
-                await Leech(task, Paths.temp_zpath, True)
+                await Zip_Handler(Paths.down_path, True, False)
+                await Leech(Paths.temp_zpath, True)
             elif is_unzip:
-                await Unzip_Handler(task, Paths.down_path, False)
-                await Leech(task, Paths.temp_unzip_path, True)
+                await Unzip_Handler(Paths.down_path, False)
+                await Leech(Paths.temp_unzip_path, True)
             elif is_dualzip:
-                await Unzip_Handler(task, Paths.down_path, False)
-                await Zip_Handler(task, Paths.temp_unzip_path, True, True)
-                await Leech(task, Paths.temp_zpath, True)
+                await Unzip_Handler(Paths.down_path, False)
+                await Zip_Handler(Paths.temp_unzip_path, True, True)
+                await Leech(Paths.temp_zpath, True)
             else:
                 if ospath.isdir(s):
-                    await Leech(task, Paths.down_path, False)
+                    await Leech(Paths.down_path, False)
                 else:
                     Transfer.total_down_size = ospath.getsize(s)
                     makedirs(Paths.temp_dirleech_path)
                     shutil.copy(s, Paths.temp_dirleech_path)
                     Messages.download_name = ospath.basename(s)
-                    await Leech(task, Paths.temp_dirleech_path, True)
+                    await Leech(Paths.temp_dirleech_path, True)
     else:
         await downloadManager(source, is_ytdl)
 
@@ -202,25 +215,27 @@ async def Do_Leech(task, source, is_dir, is_ytdl, is_zip, is_unzip, is_dualzip):
 
         # Preparing To Upload
         if is_zip:
-            await Zip_Handler(task, Paths.down_path, True, True)
-            await Leech(task, Paths.temp_zpath, True)
+            await Zip_Handler(Paths.down_path, True, True)
+            await Leech(Paths.temp_zpath, True)
         elif is_unzip:
-            await Unzip_Handler(task, Paths.down_path, True)
-            await Leech(task, Paths.temp_unzip_path, True)
+            await Unzip_Handler(Paths.down_path, True)
+            await Leech(Paths.temp_unzip_path, True)
         elif is_dualzip:
             print("Got into un doubled zip")
-            await Unzip_Handler(task, Paths.down_path, True)
-            await Zip_Handler(task, Paths.temp_unzip_path, True, True)
-            await Leech(task, Paths.temp_zpath, True)
+            await Unzip_Handler(Paths.down_path, True)
+            await Zip_Handler(Paths.temp_unzip_path, True, True)
+            await Leech(Paths.temp_zpath, True)
         else:
-            await Leech(task, Paths.down_path, True)
+            await Leech(Paths.down_path, True)
 
-    await SendLogs(task, True)
+    await SendLogs(True)
 
 
-async def Do_Mirror(task, source, is_ytdl, is_zip, is_unzip, is_dualzip):
+async def Do_Mirror(source, is_ytdl, is_zip, is_unzip, is_dualzip):
     if not ospath.exists(Paths.MOUNTED_DRIVE):
-        await cancelTask("Google Drive is NOT MOUNTED ! Stop the Bot and Run the Google Drive Cell to Mount, then Try again !")
+        await cancelTask(
+            "Google Drive is NOT MOUNTED ! Stop the Bot and Run the Google Drive Cell to Mount, then Try again !"
+        )
         return
 
     if not ospath.exists(Paths.mirror_dir):
@@ -237,16 +252,16 @@ async def Do_Mirror(task, source, is_ytdl, is_zip, is_unzip, is_dualzip):
     mirror_dir_ = ospath.join(Paths.mirror_dir, cdt_)
 
     if is_zip:
-        await Zip_Handler(task, Paths.down_path, True, True)
+        await Zip_Handler(Paths.down_path, True, True)
         shutil.copytree(Paths.temp_zpath, mirror_dir_)
     elif is_unzip:
-        await Unzip_Handler(task, Paths.down_path, True)
+        await Unzip_Handler(Paths.down_path, True)
         shutil.copytree(Paths.temp_unzip_path, mirror_dir_)
     elif is_dualzip:
-        await Unzip_Handler(task, Paths.down_path, True)
-        await Zip_Handler(task, Paths.temp_unzip_path, True, True)
+        await Unzip_Handler(Paths.down_path, True)
+        await Zip_Handler(Paths.temp_unzip_path, True, True)
         shutil.copytree(Paths.temp_zpath, mirror_dir_)
     else:
         shutil.copytree(Paths.down_path, mirror_dir_)
 
-    await SendLogs(task, False)
+    await SendLogs(False)
